d93bb4b221614b3c59f6ec45669a31e2fe207836
John Friel <john@johnfriel.net>
2024-05-02 18:46:11 -0500

    Code for a base for service layers

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,3 @@
 export { logError, handleError } from './utils/errorLogging.js';
+export { BaseActionData, ServiceError, ServiceResponse, IntentHandler } from './services/types.js';
+export { validateFormData, createErrorResponse, createSuccessResponse } from './services/base.js';
---
+++ dist/services/types.d.ts
@@ -0,0 +1,19 @@
+export interface BaseActionData<T = {}> {
+    success?: boolean;
+    data?: T;
+    errors?: {
+        [K in keyof T]?: string[];
+    } & {
+        base?: string[];
+    };
+}
+export type ServiceError = {
+    field: string;
+    message: string;
+};
+export type ServiceResponse<T> = {
+    success: boolean;
+    data: T | null;
+    errors: ServiceError[];
+};
+export type IntentHandler<T> = (formData: unknown) => Promise<ServiceResponse<T>>;
